942be1a514f26328e40b679433d891f2f879b282
Dimitri C <couwenberg@gmail.com>
2019-02-03 12:36:36 +1100

    removed app style

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { MasterLayout } from './MasterLayout';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <MasterLayout>
           <FlexStuff />
         </MasterLayout>
