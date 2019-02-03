7e4e724851ee76d7e395070c0686358be2b30bea
Dimitri C <couwenberg@gmail.com>
2019-02-03 12:39:48 +1100

    cleanup unused stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import FlexStuff from './FlexStuff'
 import { MasterLayout } from './MasterLayout';
 
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import FlexStuff from './FlexStuff'
-
 export class Header extends Component {
   render() {
     return (
